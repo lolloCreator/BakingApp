56a2cdf4c5f7c6d3d62f13036533f4bbaa3d3402
jk205 <juan.karstens@gmail.com>
2017-10-02 15:42:18 +0200

    add toolbar

--- BakingApp/app/src/main/java/org/jkarsten/bakingapp/bakingapp/stepdetail/StepDetailActivity.java
+++ BakingApp/app/src/main/java/org/jkarsten/bakingapp/bakingapp/stepdetail/StepDetailActivity.java
@@ -2,6 +2,7 @@ package org.jkarsten.bakingapp.bakingapp.stepdetail;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 
 import org.jkarsten.bakingapp.bakingapp.OnDualPaneInteractionListener;
 import org.jkarsten.bakingapp.bakingapp.R;
@@ -15,6 +16,12 @@ public class StepDetailActivity extends AppCompatActivity implements OnDualPaneI
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_step_detail);
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setHomeButtonEnabled(true);
     }
 
     @Override
